a87c0c192a57b9f2c431fa9cbd258fcfe5b28159
Sunil Yadav <sunil.yadav@harman.com>
2021-07-03 12:29:42 +0530

    first commit

---
+++ SunCoreAPI/SunCoreAPI.h
@@ -0,0 +1,18 @@
+//
+//  SunCoreAPI.h
+//  SunCoreAPI
+//
+//  Created by skyadav on 03/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SunCoreAPI.
+FOUNDATION_EXPORT double SunCoreAPIVersionNumber;
+
+//! Project version string for SunCoreAPI.
+FOUNDATION_EXPORT const unsigned char SunCoreAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SunCoreAPI/PublicHeader.h>
+
+
