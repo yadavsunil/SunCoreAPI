e905335620559d9d4fbf5c26127cc29018e9169c
Sunil Yadav <sunil.yadav@harman.com>
2021-07-03 12:49:54 +0530

    Added test cases

---
+++ SunCoreAPI/SunCoreAPI.swift
@@ -0,0 +1,21 @@
+//
+//  SunCoreAPI.swift
+//  SunCoreAPI
+//
+//  Created by skyadav on 03/07/21.
+//
+
+import Foundation
+public final class SunCoreAPI {
+
+    let name = "SunCoreAPI"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SunCoreAPITests/SunCoreAPITests.swift
+++ SunCoreAPITests/SunCoreAPITests.swift
@@ -9,25 +9,24 @@ import XCTest
 @testable import SunCoreAPI
 
 class SunCoreAPITests: XCTestCase {
+    
+    var sunCoreAPI: SunCoreAPI?
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        sunCoreAPI = SunCoreAPI()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAdd() throws {
+        XCTAssertEqual(sunCoreAPI?.add(a: 1, b: 1), 2)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSub() {
+        XCTAssertEqual(sunCoreAPI?.sub(a: 2, b: 1), 1)
     }
 
 }
